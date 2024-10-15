017802b98b8fdab7400d48552a682621a7bbcc34
Aditya Verma <148361302+AdityaBVerma@users.noreply.github.com>
2024-10-15 23:58:16 +0530

    traversals in BST

--- 03trees/BSTImplementation.c
+++ 03trees/BSTImplementation.c
@@ -14,6 +14,8 @@ struct Node* newNode ( int item) {
     return newNode;
 }
 
+// helper function to get the successor(in it's place) of a node with both children
+
 struct Node* getSuccessor (struct Node* curr) {
     curr = curr->right;
     while (curr!=NULL && curr->left!=NULL){
@@ -21,6 +23,8 @@ struct Node* getSuccessor (struct Node* curr) {
     }
     return curr;
 }
+
+// insertion of a new Node in the BST
 
 struct Node* insertNode (struct Node* node, int x){
     if (node == NULL) return newNode(x);
@@ -34,6 +38,8 @@ struct Node* insertNode (struct Node* node, int x){
     }
     return node;
 }
+
+// deletion of a new Node in the BST
 
 struct Node* delNode ( struct Node* root, int x){
     if (root == NULL ) return root;
@@ -63,11 +69,29 @@ struct Node* delNode ( struct Node* root, int x){
     return root;
 }
 
+// traversal in BST
+
 void inorder (struct Node* node) {
     if (node!=NULL) {
         inorder(node->left);
         printf("%d ", node->key);
         inorder(node->right);
+    }
+}
+
+void preorder (struct Node* node) {
+    if (node != NULL) {
+        printf("%d ", node->key);
+        preorder(node->left);
+        preorder(node->right);
+    }
+}
+
+void postorder (struct Node* node) {
+    if (node != NULL) {
+        postorder(node->left);
+        postorder(node->right);
+        printf("%d ", node->key);
     }
 }
 
@@ -81,10 +105,19 @@ int main() {
     root = insertNode(root, 70);
     root = insertNode(root, 40);
     root = insertNode(root, 80);
+    
     inorder(root);
+    printf("\n");
+    preorder(root);
+    printf("\n");
+    postorder(root);
+    printf("\n");
+
     root = delNode(root, 60);
     printf("\n");
     inorder(root);
+    
+    
     
     return 0;
 }
